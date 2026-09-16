f16fc74f92ec8d24006b5dfc50f5e80327ffbf92
behruztagayev59-cloud <behruztagayev59@gmail.com>
2026-09-16 22:08:16 +1000

    Add AI task: Vaqtni birliklarga ajratish

---
+++ ai_topshiriqlar/Vaqtni_birliklarga_ajratish.py
@@ -0,0 +1,14 @@
+# Vaqtni birliklarga ajratish
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 30
+# Aziz Academy — AI Topshiriq
+
+# Yechimingizni shu yerga yozing
+# Kirish: input(), chiqish: print()
+T = int(input())
+soat = T // 60
+daqiqa = T % 60
+
+print(soat)
+print(daqiqa)
