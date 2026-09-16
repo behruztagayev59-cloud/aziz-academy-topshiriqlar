3810778c69308c5ef1d2c44279f1c1a4cd76d913
behruztagayev59-cloud <behruztagayev59@gmail.com>
2026-09-16 22:49:42 +1000

    Add: Akkaunt holati bayroqlari [behruztagayev59-cloud]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
@@ -0,0 +1,6 @@
+royxatdan_otgan = True
+email_tasdiqlangan = False
+bloklangan = False
+print(f"Ro'yxatdan o'tgan: {royxatdan_otgan}")
+print(f"Email tasdiqlangan: {email_tasdiqlangan}")
+print(F"Bloklangan: {bloklangan}")
